2936b62454ad98f2dc7029b57fb2880b9822c7f8
ted <tngeene27@gmail.com>
2020-12-20 19:53:45 +0300

    added controls for consent form viewing

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from portal.models import ConsentForm
 
 class IndexTemplateView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["consent_form"] = ConsentForm.objects.last()
+        return context
+
